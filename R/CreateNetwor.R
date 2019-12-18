b062911162637780d3cd30960a28d733a874a628
jpablo91 <jpgo@ucdavis.edu>
2019-12-17 17:24:25 -0800

    Updated CreateNetwork

--- R/CreateNetwor.R
+++ R/CreateNetwor.R
@@ -1,21 +1,23 @@
 # FUnction to get the contacts
 CreateNetwork <- function(DF, DTh, coords = c("X", "Y"), DateTime = "DateTime", ID = "ID"){
   ### Test data
-  # DF <- GPSc; ID <- "CollarID"; DateTime <- "DateTime"; coords <- c("X", "Y"); DTh = 1
-  # DTh <- 10
+  # DF <- data.frame(GPSc); ID <- "CollarID"; DateTime <- "DateTime"; coords <- c("X", "Y"); DTh = 1
+  # DF <- GPSc; DateTime = "date"; ID = "id"; coords = c("Longitude", "Latitude")
+  # DTh <- 1
   TimePoints <- unique(DF[DateTime]) # Get unique timepoints
   
   D_DF <- data.frame(Var1 = NA, Var2 = NA, X.x = NA, Y.x = NA, X.y = NA, Y.y = NA, Distance = NA, DateTime = TimePoints[1,]) # Create a data frame to fill with the data
+  # D_DF <- matrix(ncol = 8)
   
   # Loop to get the contacts
   for(i in 1:nrow(TimePoints)){
     # i <- 1
-    DF_i <- DF %>%
-      filter(DateTime == TimePoints[i,])
+    DF_i <- DF[which(DF[,DateTime] == TimePoints[i,]),]
+    
     SP_i <-  DF_i %>%
       st_as_sf(coords = coords)
     
-    D_DF_i <- expand.grid(SP_i$CollarID, SP_i$CollarID) %>% # Create a data frame for all combinations of possible Origin-Destination
+    D_DF_i <- expand.grid(DF_i[, ID], DF_i[, ID]) %>% # Create a data frame for all combinations of possible Origin-Destination
       left_join(DF_i[,c(ID, coords)], by = c("Var1" = ID)) %>%
       left_join(DF_i[,c(ID, coords)], by = c("Var2" = ID))
     
@@ -30,6 +32,8 @@ CreateNetwork <- function(DF, DTh, coords = c("X", "Y"), DateTime = "DateTime", 
       mutate(DateTime = TimePoints[i,]) %>%
       filter(Var1 != Var2) %>% # Exclude the ones that are same origin-destination
       filter(Distance <= DTh) # Filter to the ones that are under the Spatial treshold
+    
+    colnames(D_DF_i) <- colnames(D_DF)
     
     D_DF <- rbind(D_DF, D_DF_i)
   }
