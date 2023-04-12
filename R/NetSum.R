1f5ebb7758e83d187c366608461b0cafe7f990a4
Jose Pablo Gomez <josepablogomez@Pablos-MacBook-Pro.local>
2023-04-12 10:25:56 -0700

    added NetSum function

---
+++ R/NetSum.R
@@ -0,0 +1,27 @@
+#' Function to get a network summary
+#' 
+#' @param x a igraph or tbl grpah object.
+#' @export
+
+NetSum <- function(x){
+  x = tidygraph::as_tbl_graph(x)
+  data.frame(
+    Edges = x %E>% data.frame() %>% nrow(),
+    Nodes = length(x),
+    Density = igraph::graph.density(x),
+    Components = igraph::components(x)$no,
+    Diameter = igraph::diameter(x),
+    AvgPathLength = igraph::average.path.length(x),
+    # Assortativity = igraph::assortativity(x),
+    Transitivity = igraph::transitivity(x, type = 'global'),
+    MeanDegree = igraph::degree(x) %>% mean(),
+    MedianDegree = igraph::degree(x) %>% median(),
+    AvgNbs = igraph::neighborhood.size(x) %>% mean(),
+    Modularity = cluster_walktrap(x) %>% modularity(),
+    CentDegree = centralization.degree(x)$centralization,
+    CentBetweenness = centralization.betweenness(x)$centralization,
+    CentCloseness = centralization.closeness(x)$centralization
+  ) %>% t() %>% 
+    round(., 4) %>% 
+    data.frame()
+}
